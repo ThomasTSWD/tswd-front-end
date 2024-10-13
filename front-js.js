40f4aecfed151fedc0664511bb77a222d68c3dd8
Tom TSWD <54892623+ThomasTSWD@users.noreply.github.com>
2024-10-13 20:32:25 +0200

    Update front-js.js

--- front-js.js
+++ front-js.js
@@ -1,5 +1,25 @@
-jQuery(document).ready(function($) {
+jQuery(document).ready(function ($) {
 
+	/****** IMG HOVER */
+
+	$("img").hover(function () {
+		var title = $(this).attr("title");
+		$(this).attr("tmp_title", title);
+		$(this).attr("title", "");
+	}, function () {
+		$(this).attr("title", $(this).attr("tmp_title"));
+	});
+
+	/******** TSWD Year**********/
+
+	const tswdYear = new Date().getFullYear();
+	$('.tswd-year').text(tswdYear);
+
+	/********* CF7 *********/
+
+	$(document).on('wpcf7mailsent', function (event) {
+		var $form = $(event.target);
+		$form.find('> :not(.wpcf7-response-output)').fadeOut(500);
+	});
 
 });
-//---jQuery ends--------------------------------------	
